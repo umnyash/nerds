d38a3b5b21420ac447f096d755337cae3b639e2a
Rustam <rustam17.sb@gmail.com>
2023-01-24 18:58:09 +0300

    fix: remove untrusted plugins

--- tasks/scss.js
+++ tasks/scss.js
@@ -11,8 +11,6 @@ import autoprefixer from 'gulp-autoprefixer';
 import csso from 'gulp-csso';
 import rename from 'gulp-rename';
 import size from 'gulp-size';
-import shorthand from 'gulp-shorthand';
-import groupCssMediaQueries from 'gulp-group-css-media-queries';
 import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 import sassGlob from 'gulp-sass-glob';
@@ -31,7 +29,6 @@ export default () => {
     .pipe(sassGlob())
     .pipe(sass())
     .pipe(autoprefixer())
-    .pipe(groupCssMediaQueries())
     .pipe(size({ title: 'main.css' }))
     .pipe(gulp.dest(path.scss.dest, { sourcemaps: app.isDev }))
     .pipe(rename({ suffix: '.min' }))
